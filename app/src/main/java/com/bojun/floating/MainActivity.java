ee80e430557a6744c4af82fd77b0729bb163ac04
demoned <atl1314@qq.com>
2020-03-18 15:32:38 +0800

    FloatingMenu菜单

---
+++ app/src/main/java/com/bojun/floating/MainActivity.java
@@ -0,0 +1,51 @@
+package com.bojun.floating;
+
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionMenu menuRed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        menuRed = (FloatingActionMenu) findViewById(R.id.menu_red);
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        FloatingActionButton fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+        menuRed.setClosedOnTouchOutside(true);
+        menuRed.setOnMenuButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (menuRed.isOpened()) {
+                    Toast.makeText(getApplicationContext(), menuRed.getMenuButtonLabelText(), Toast.LENGTH_SHORT).show();
+                }
+
+                menuRed.toggle(true);
+            }
+        });
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "fab1", Toast.LENGTH_SHORT).show();
+            }
+        });
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "fab2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        fab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "fab3", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
