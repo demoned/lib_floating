94a3c2d5f8717eb60e7167ebe46974734c07148a
demoned <atl1314@qq.com>
2020-03-18 15:40:18 +0800

    FloatingMenu菜单

--- app/src/main/java/com/bojun/floating/MainActivity.java
+++ app/src/main/java/com/bojun/floating/MainActivity.java
@@ -2,7 +2,6 @@ package com.bojun.floating;
 
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
